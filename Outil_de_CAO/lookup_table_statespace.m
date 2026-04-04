0445027c877f4fb250d7d9cacc3d90105890e28b
Tommy Murray <tomur@ulaval.ca>
2026-04-04 10:17:57 -0400

    petite modif

--- Outil_de_CAO/lookup_table_statespace.m
+++ Outil_de_CAO/lookup_table_statespace.m
@@ -6,10 +6,10 @@ sat_proc_max = 1;
 sat_proc_min = -1;
 
 %% Paramètres de La poutre et du matériau
-b = 7.1e-2;      % Base 6 cm (selon ton code: 7.1 cm)
-h = 1.5e-3;      % Hauteur 1.56 mm (1.5 mm ici)
-L = 24.3e-2;     % Longueur 24.6 cm (24.3 cm ici)
-E = 24e9;        % Module de Young 24 GPa
+b = 7.1e-2;      % Base
+h = 1.5875e-3;   % Hauteur
+L = 24.3e-2;     % Longueur
+E = 18.6e9;      % Module de Young
 dens = 1850;     % Densité Kg/m^3
 mu = dens*b*h;   % Masse linéique kg/m (rho * A_ss)
 A_b = b*h;       % Aire de la section
@@ -29,8 +29,8 @@ pos_actionneur_et_masse = 0.146; % Position en mètre
 masse_bobine_et_plaque = 40/1000;
 masse_aimant = 1/1000; % Aimant au bout
 
-alpha = 1.4;          % Coefficient de Rayleigh pour l'amortissement externe (résistance à l'air)
-beta = 0.0001;        % Coefficient de Rayleigh pour l'amortissement interne (forces dans le matériel)
+alpha = 5;          % Coefficient de Rayleigh pour l'amortissement externe (résistance à l'air)
+beta = 0.005;        % Coefficient de Rayleigh pour l'amortissement interne (forces dans le matériel)
 
 %% 0. Initialiser les matrices 3D pour les tables
 % Plage de masse de 0 à 100g par bonds de 1g
